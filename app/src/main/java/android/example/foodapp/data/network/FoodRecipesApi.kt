24b2db84d3d9d655d99d46327ca6dcb602757b7d
Soe Moe Aung <ahshan061@gmail.com>
2023-02-15 11:06:36 +0800

    FoodApp Project

--- app/src/main/java/android/example/foodapp/data/network/FoodRecipesApi.kt
+++ app/src/main/java/android/example/foodapp/data/network/FoodRecipesApi.kt
@@ -6,8 +6,14 @@ import retrofit2.http.GET
 import retrofit2.http.QueryMap
 
 interface FoodRecipesApi {
+
     @GET("/recipes/complexSearch")
      suspend fun getRecipes(
         @QueryMap queries : Map<String, String>
     ):Response<FoodRecipe>
+
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+         @QueryMap searchQuery : Map<String, String>
+     ):Response<FoodRecipe>
 }
